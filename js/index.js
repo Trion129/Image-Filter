e811907d67611a0f51f696baea32e2e47b8fec3e
Kunal Gupta <kunal@github.com>
2016-10-11 01:42:01 +0530

    User can add the images of their choice to filter but with a glitch

--- js/index.js
+++ js/index.js
@@ -5,16 +5,16 @@ var button1, button2, button3, button4, button5;
 var urlImage = 'http://i.imgur.com/Zf5Im77.jpg';
 
 
-$('#urlImage').on('keyup', function () {
-  urlImage = $(this).val();
-  console.log(urlImage);
-})
+$('#urlImage').on('keyup', preload)
 
-function preload() {
-  OurImage = loadImage(urlImage);
+function preload() {  
+  if($('#urlImage').val()){        
+    urlImage = $('#urlImage').val();
+  }
+  OurImage = loadImage(urlImage);      
 }
 
-function renderImage() {
+function renderImage() {  
   image(OurImage, 0, 0, OurImage.width, OurImage.height, 0, 0,
     590, 320);
   loadPixels();
@@ -22,8 +22,8 @@ function renderImage() {
   updatePixels();
 }
 
-function setup() {
-  var canvas = createCanvas(500,500);
+function setup() {     
+  var canvas = createCanvas(500,500);  
   canvas.parent('myCanvas');
   createButtons();
   renderImage();
